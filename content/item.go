f8918babe3730b2f47b3a25c5e4d3b892afd8cc6
Steve Manuel <nilslice@gmail.com>
2016-10-17 19:30:46 -0700

    testing int64 type for timestamp & update

--- content/item.go
+++ content/item.go
@@ -1,11 +1,9 @@
 package content
 
-import "time"
-
 // Item should only be embedded into content type structs.
 type Item struct {
-	ID        int       `json:"id"`
-	Slug      string    `json:"slug"`
-	Timestamp time.Time `json:"timestamp"`
-	Updated   time.Time `json:"updated"`
+	ID        int    `json:"id"`
+	Slug      string `json:"slug"`
+	Timestamp int64  `json:"timestamp"`
+	Updated   int64  `json:"updated"`
 }
