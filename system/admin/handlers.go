7b55b66d905398d2521e881c28db72c01bd93dbf
Steve Manuel <nilslice@gmail.com>
2016-12-01 11:44:55 -0800

    adding example html for admin content pagination

--- system/admin/handlers.go
+++ system/admin/handlers.go
@@ -911,7 +911,21 @@ func postsHandler(res http.ResponseWriter, req *http.Request) {
 
 	html += `<ul class="posts row">`
 
-	b.Write([]byte(`</ul></div></div>`))
+	b.Write([]byte(`</ul>`))
+
+	pagination := `
+	<ul class="pagination">
+		<li class="disabled"><a href="#!"><i class="material-icons">chevron_left</i></a></li>
+		<li class="active"><a href="#!">1</a></li>
+		<li class="waves-effect"><a href="#!">2</a></li>
+		<li class="waves-effect"><a href="#!">3</a></li>
+		<li class="waves-effect"><a href="#!">4</a></li>
+		<li class="waves-effect"><a href="#!">5</a></li>
+		<li class="waves-effect"><a href="#!"><i class="material-icons">chevron_right</i></a></li>
+	</ul>
+	`
+
+	b.Write([]byte(pagination + `</div></div>`))
 
 	script := `
 	<script>
