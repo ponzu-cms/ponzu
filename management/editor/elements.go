47d99bc7edb9e59ac07a95807d0f34b3b913680e
Steve Manuel <nilslice@gmail.com>
2016-12-05 20:13:52 -0800

    testing fix after changing class serializer

--- management/editor/elements.go
+++ management/editor/elements.go
@@ -375,7 +375,7 @@ func Tags(fieldName string, p interface{}, attrs map[string]string) []byte {
 				
 				var input = $('<input>');
 				input.attr({
-					class: '__ponzu-tag '+chip.tag,
+					class: '__ponzu-tag '+chip.tag.split(' ').join('__'),
 					name: '` + name + `.'+String(tags.find('input[type=hidden]').length),
 					value: chip.tag,
 					type: 'hidden'
@@ -386,8 +386,7 @@ func Tags(fieldName string, p interface{}, attrs map[string]string) []byte {
 
 			chips.on('chip.delete', function(e, chip) {
 				// convert tag string to class-like selector "some tag" -> ".some.tag"
-				console.log(chip.id);
-				var sel = '.__ponzu-tag.' + chip.tag.split(' ').join('.');
+				var sel = '.__ponzu-tag.' + chip.tag.split(' ').join('__');
 				chips.parent().find(sel).remove();
 
 				// iterate through all hidden tag inputs to re-name them with the correct ` + name + `.index
