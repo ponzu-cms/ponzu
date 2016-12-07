c99dcd123e886e7c62c913074c5c04ae3acc3272
Steve Manuel <nilslice@gmail.com>
2016-12-06 18:17:24 -0800

    re-order middleware so Record is called even if cache is active

--- system/admin/server.go
+++ system/admin/server.go
@@ -7,6 +7,7 @@ import (
 	"path/filepath"
 
 	"github.com/bosssauce/ponzu/system/admin/user"
+	"github.com/bosssauce/ponzu/system/api"
 	"github.com/bosssauce/ponzu/system/db"
 )
 
@@ -48,5 +49,5 @@ func Run() {
 	// even if the API server is not running. Otherwise, images/files uploaded
 	// through the editor will not load within the admin system.
 	uploadsDir := filepath.Join(pwd, "uploads")
-	http.Handle("/api/uploads/", db.CacheControl(http.StripPrefix("/api/uploads/", http.FileServer(restrict(http.Dir(uploadsDir))))))
+	http.Handle("/api/uploads/", api.Record(db.CacheControl(http.StripPrefix("/api/uploads/", http.FileServer(restrict(http.Dir(uploadsDir)))))))
 }
--- system/api/server.go
+++ system/api/server.go
@@ -8,11 +8,11 @@ import (
 
 // Run adds Handlers to default http listener for API
 func Run() {
-	http.HandleFunc("/api/types", db.CacheControl(CORS(Record(typesHandler))))
+	http.HandleFunc("/api/types", Record(db.CacheControl(CORS(typesHandler))))
 
-	http.HandleFunc("/api/contents", db.CacheControl(CORS(Record(contentsHandler))))
+	http.HandleFunc("/api/contents", Record(db.CacheControl(CORS(contentsHandler))))
 
-	http.HandleFunc("/api/content", db.CacheControl(CORS(Record(contentHandler))))
+	http.HandleFunc("/api/content", Record(db.CacheControl(CORS(contentHandler))))
 
-	http.HandleFunc("/api/content/external", db.CacheControl(CORS(Record(externalContentHandler))))
+	http.HandleFunc("/api/content/external", Record(db.CacheControl(CORS(externalContentHandler))))
 }
