a20025b452934069fc6c39949d7b9e123a503c9c
Steve Manuel <nilslice@gmail.com>
2016-12-07 09:32:46 -0800

    testing cache control exec from CORS

--- system/api/server.go
+++ system/api/server.go
@@ -1,18 +1,14 @@
 package api
 
-import (
-	"net/http"
-
-	"github.com/bosssauce/ponzu/system/db"
-)
+import "net/http"
 
 // Run adds Handlers to default http listener for API
 func Run() {
-	http.HandleFunc("/api/types", Record(db.CacheControl(CORS(typesHandler))))
+	http.HandleFunc("/api/types", Record((CORS(typesHandler))))
 
-	http.HandleFunc("/api/contents", Record(db.CacheControl(CORS(contentsHandler))))
+	http.HandleFunc("/api/contents", Record((CORS(contentsHandler))))
 
-	http.HandleFunc("/api/content", Record(db.CacheControl(CORS(contentHandler))))
+	http.HandleFunc("/api/content", Record((CORS(contentHandler))))
 
-	http.HandleFunc("/api/content/external", Record(db.CacheControl(CORS(externalContentHandler))))
+	http.HandleFunc("/api/content/external", Record((CORS(externalContentHandler))))
 }
