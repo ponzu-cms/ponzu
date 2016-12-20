9b7ee17fe3b43cedb151760215644591a6971d5a
Steve Manuel <nilslice@gmail.com>
2016-12-20 09:03:13 -0800

    fixing textarea element UI

--- management/editor/elements.go
+++ management/editor/elements.go
@@ -32,6 +32,15 @@ func Input(fieldName string, p interface{}, attrs map[string]string) []byte {
 // The `fieldName` argument will cause a panic if it is not exactly the string
 // form of the struct field that this editor input is representing
 func Textarea(fieldName string, p interface{}, attrs map[string]string) []byte {
+	// add materialize css class to make UI correct
+	className := "materialize-textarea"
+	if _, ok := attrs["class"]; ok {
+		class := attrs["class"]
+		attrs["class"] = class + " " + className
+	} else {
+		attrs["class"] = className
+	}
+
 	e := newElement("textarea", attrs["label"], fieldName, p, attrs)
 
 	return domElement(e)
