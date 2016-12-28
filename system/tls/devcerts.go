e13cbb199623a8462297bebb4a22952418cc046d
Steve Manuel <nilslice@gmail.com>
2016-12-27 17:51:26 -0800

    adding CA option back for testing

--- system/tls/devcerts.go
+++ system/tls/devcerts.go
@@ -102,9 +102,8 @@ func setupDev() {
 		}
 	}
 
-	hosts = []string{"localhost", "0.0.0.0"}
 	// make all certs CA
-	// template.IsCA = true
+	template.IsCA = true
 	template.KeyUsage |= x509.KeyUsageCertSign
 
 	derBytes, err := x509.CreateCertificate(rand.Reader, &template, &template, publicKey(priv), priv)
