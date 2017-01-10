159acf2ebbe6688f4f24deedc3e9ca4880a63b3b
Steve Manuel <nilslice@gmail.com>
2017-01-10 11:16:03 -0800

    pulling db connection creation into package init not exported Init() initialization func

--- system/db/init.go
+++ system/db/init.go
@@ -13,6 +13,14 @@ import (
 
 var store *bolt.DB
 
+func init() {
+	var err error
+	store, err = bolt.Open("system.db", 0666, nil)
+	if err != nil {
+		log.Fatalln(err)
+	}
+}
+
 // Close exports the abillity to close our db file. Should be called with defer
 // after call to Init() from the same place.
 func Close() {
@@ -24,13 +32,7 @@ func Close() {
 
 // Init creates a db connection, initializes db with required info, sets secrets
 func Init() {
-	var err error
-	store, err = bolt.Open("system.db", 0666, nil)
-	if err != nil {
-		log.Fatalln(err)
-	}
-
-	err = store.Update(func(tx *bolt.Tx) error {
+	err := store.Update(func(tx *bolt.Tx) error {
 		// initialize db with all content type buckets & sorted bucket for type
 		for t := range item.Types {
 			_, err := tx.CreateBucketIfNotExists([]byte(t))
