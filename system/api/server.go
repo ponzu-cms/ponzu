a645a66ecd2d68cb6719cb22bdb91a0480be8659
Steve Manuel <nilslice@gmail.com>
2017-01-16 15:21:52 -0800

    adding gzip compression to api endpoints

--- system/api/server.go
+++ system/api/server.go
@@ -4,9 +4,9 @@ import "net/http"
 
 // Run adds Handlers to default http listener for API
 func Run() {
-	http.HandleFunc("/api/contents", Record(CORS(contentsHandler)))
+	http.HandleFunc("/api/contents", Record(CORS(Gzip(contentsHandler))))
 
-	http.HandleFunc("/api/content", Record(CORS(contentHandler)))
+	http.HandleFunc("/api/content", Record(CORS(Gzip(contentHandler))))
 
 	http.HandleFunc("/api/content/external", Record(externalContentHandler))
 }
