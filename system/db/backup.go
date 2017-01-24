3a897e4db97cc6f5e47f395662499402eb4c2bda
Steve <nilslice@gmail.com>
2017-01-24 10:35:18 -0800

    [core] System backups (uploads, system.db, analytics.db) (#42)

---
+++ system/db/backup.go
@@ -0,0 +1,26 @@
+package db
+
+import (
+	"fmt"
+	"net/http"
+	"time"
+
+	"github.com/boltdb/bolt"
+)
+
+// Backup writes a snapshot of the system.db database to an HTTP response
+func Backup(res http.ResponseWriter) error {
+	err := store.View(func(tx *bolt.Tx) error {
+		ts := time.Now().Unix()
+		disposition := `attachment; filename="system-%d.db.bak"`
+
+		res.Header().Set("Content-Type", "application/octet-stream")
+		res.Header().Set("Content-Disposition", fmt.Sprintf(disposition, ts))
+		res.Header().Set("Content-Length", fmt.Sprintf("%d", int(tx.Size())))
+
+		_, err := tx.WriteTo(res)
+		return err
+	})
+
+	return err
+}
