6ea648ae4306e64f5562ca363e0cdea494ba8d46
Steve Manuel <nilslice@gmail.com>
2017-02-09 09:37:44 -0800

    remove req.URL.Path from the push target so non-content paths like uploads can be pushed too

--- system/api/push.go
+++ system/api/push.go
@@ -23,7 +23,7 @@ func push(res http.ResponseWriter, req *http.Request, pt func() interface{}, dat
 			for i := range values {
 				val := values[i]
 				val.ForEach(func(k, v gjson.Result) bool {
-					err := pusher.Push(req.URL.Path+v.String(), nil)
+					err := pusher.Push(v.String(), nil)
 					if err != nil {
 						log.Println("Error during Push of value:", v.String())
 					}
