aa0c758409dc2673b269c2da395c616f336ac655
Steve Manuel <nilslice@gmail.com>
2018-05-09 12:11:22 -0600

    update CI code to run --dev if on ponzu-dev branch

--- .circleci/test-setup.sh
+++ .circleci/test-setup.sh
@@ -7,19 +7,21 @@ set -ex
 # Install Ponzu CMS
 go get -u github.com/ponzu-cms/ponzu/...
 
-
 # test install
 ponzu
 
-
 # create a project and generate code
-ponzu new github.com/ponzu-cms/ci/test-project
+if [ $CIRCLE_BRANCH = "ponzu-dev" ]; then
+        # create new project using the ponzu-dev branch
+        ponzu new --dev github.com/ponzu-cms/ci/test-project
+else 
+        ponzu new github.com/ponzu-cms/ci/test-project
+fi
 
 cd /go/src/github.com/ponzu-cms/ci/test-project
 
 ponzu gen content person name:string hashed_secret:string
 ponzu gen content message from:@person,hashed_secret to:@person,hashed_secret
-
 
 # build and run dev http/2 server with TLS
 ponzu build
