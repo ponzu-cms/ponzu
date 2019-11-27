8db1e913253ef8188ef44021586e6c59c2d15109
Steve Manuel <nilslice@gmail.com>
2019-11-27 00:12:03 -0700

    ci: try install of ponzu branch

--- .circleci/test-setup.sh
+++ .circleci/test-setup.sh
@@ -5,11 +5,12 @@
 set -ex 
 
 # Install Ponzu CMS
-go install ./...
+go get github.com/ponzu-cms/ponzu/...
 
 # test install
 ponzu
 
+PROJ_PATH=github.com/ponzu-cms/ci/test-project
 # create a project and generate code
 if [ $CIRCLE_BRANCH = "ponzu-dev" ]; then
         # ensure we have the latest from ponzu-dev branch
@@ -17,13 +18,16 @@ if [ $CIRCLE_BRANCH = "ponzu-dev" ]; then
         git checkout ponzu-dev
         git pull origin ponzu-dev
 
+        # install ponzu-dev branch of CLI
+        go install ./...
+
         # create new project using the ponzu-dev branch
-        ponzu new --dev github.com/ponzu-cms/ci/test-project
+        ponzu new --dev ${PROJ_PATH}
 else 
-        ponzu new github.com/ponzu-cms/ci/test-project
+        ponzu new ${PROJ_PATH}
 fi
 
-cd /go/src/github.com/ponzu-cms/ci/test-project
+cd /go/src/${PROJ_PATH}
 
 ponzu gen content person name:string hashed_secret:string
 ponzu gen content message from:@person,hashed_secret to:@person,hashed_secret
